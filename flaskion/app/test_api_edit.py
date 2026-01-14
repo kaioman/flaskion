448ec909e512e3b84f5c390177163fc5aea9f18b
kaioman <kajin0318@gmail.com>
2026-01-14 21:47:06 +0900

    image_editを外部から呼び出すサンプルコード作成

---
+++ flaskion/app/test_api_edit.py
@@ -0,0 +1,43 @@
+import os
+import base64
+import requests
+from pycorex.gemini_client import GeminiClient
+
+API_KEY = input("Uwgen API Keyを入力してください:").strip()
+ENDPOINT = "http://localhost:5100/api/v1/image_edit"
+
+headers = {
+    "Authorization": f"Uwgen {API_KEY}"
+}
+
+payload = {
+    "prompt":"ポーズや構図はそのままで、服装を女子高生風にして",
+    "model": GeminiClient.GeminiModel.GEMINI_3_0_PRO_IMAGE_PREVIEW.value,
+    "resolution": GeminiClient.ImageSize.TWO_K.value,
+    "aspect": GeminiClient.AspectRatio.SQUARE.value,
+    "safety_filter": GeminiClient.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT.value,
+    "safety_level": GeminiClient.SafetyFilterLevel.BLOCK_ONLY_HIGH.value
+}
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(base_dir, "hero_02.png")
+
+files = {
+    "sourceImage": open(image_path, "rb")
+}
+
+res = requests.post(ENDPOINT, data=payload, files=files, headers=headers)
+json_data = res.json()
+
+images = json_data["data"]["generated"]["images"]
+
+output_dir = os.path.dirname(os.path.abspath(__file__))
+
+for idx, img_b64 in enumerate(images):
+    img_bytes = base64.b64decode(img_b64)
+    file_path = os.path.join(output_dir, f"generated_{idx}.png")
+    
+    with open(file_path, "wb") as f:
+        f.write(img_bytes)
+    
+    print(f"Saved: {file_path}")
